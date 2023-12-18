7a68f66df01666900a20ea74cf565b4111fd214a
Matt Chaulklin <mchaulklin@gmail.com>
2023-12-18 15:12:45 -0500

    Researching

--- src/BenchmarkDotNet/Running/BenchmarkRunnerDirty.cs
+++ src/BenchmarkDotNet/Running/BenchmarkRunnerDirty.cs
@@ -86,10 +86,34 @@ namespace BenchmarkDotNet.Running
 
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static Summary RunWithDirtyAssemblyResolveHelper(Type type, IConfig? config, string[]? args)
-            => (args == null
-                ? BenchmarkRunnerClean.Run(new[] { BenchmarkConverter.TypeToBenchmarks(type, config) })
-                : new BenchmarkSwitcher(new[] { type }).RunWithDirtyAssemblyResolveHelper(args, config, false))
-                .Single();
+        {
+            // Set up Ctrl+C event handler
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                Console.WriteLine("Ctrl+C pressed. Cleaning up...");
+                // Perform necessary clean-up
+                //var powerManager = new PowerManagementApplier(); //need logger here
+                //PowerManagementApplier.Dispose();
+                eventArgs.Cancel = true; // Prevent the process from terminating immediately
+            };
+
+            try
+            {
+                return (args == null
+                    ? BenchmarkRunnerClean.Run(new[] { BenchmarkConverter.TypeToBenchmarks(type, config) })
+                    : new BenchmarkSwitcher(new[] { type }).RunWithDirtyAssemblyResolveHelper(args, config, false))
+                    .Single();
+            }
+            finally
+            {
+                // Clean up the event handler
+                Console.CancelKeyPress -= (sender, eventArgs) =>
+                {
+                    // Event handler logic
+                };
+            }
+        }
+
 
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static Summary RunWithDirtyAssemblyResolveHelper(Type type, MethodInfo[] methods, IConfig? config = null)
